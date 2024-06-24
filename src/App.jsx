f14bac000c1d2d24c3b52b7e6583bc8de228b41a
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-06-24 19:21:10 +0300

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1> Muhammed Emin Beköz To Do App</h1>
+    </div>
+  );
+}
+
+export default Header;
