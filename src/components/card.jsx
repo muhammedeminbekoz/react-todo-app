0b302f66c805175177c1174f54a174f4ba23c603
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-06-28 16:10:43 +0300

    refactor: Update Card component to handle deleting todos

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,23 +4,25 @@ import React from "react";
 import { useState } from "react";
 import { MdDelete, MdEdit } from "react-icons/md";
 
-function Card(props) {
+function Card({ todo, deleteTodo }) {
   const [textState, setTextState] = useState("");
-  const [textContent, setTextContet] = useState("");
 
   function handleDoneClick() {
     textState == "" ? setTextState("line-through") : setTextState("");
+  }
+  function handleDeleteClick() {
+    deleteTodo(todo);
   }
 
   return (
     <div className="flex items-center justify-center mt-5">
       <div className="border-2 rounded border-green-500 w-1/2 p-2 flex  justify-between items-center ">
-        <span className={textState}>{props.todo} </span>
+        <span className={textState}> {todo.content} </span>
         <div className="flex space-x-2">
           <button className="p-1 text-xl active:scale-95 ">
             <MdEdit />
           </button>
-          <button className="p-1 text-xl active:scale-95">
+          <button className="p-1 text-xl active:scale-95" onClick={handleDeleteClick}>
             <MdDelete />
           </button>
         </div>
