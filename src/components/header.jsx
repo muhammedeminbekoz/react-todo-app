9fe527e3c59020724f2ff45004672826f57f0bd3
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-07-01 12:33:04 +0300

    feat: Add a header component that includes sticky behavior and a scroll-to-top functionality.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,9 +2,18 @@
 import React from "react";
 
 function Header() {
+  const scrollToUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div>
-      <h1> Muhammed Emin Beköz To Do App</h1>
+    <div className="sticky top-0 bg-blue-500 p-4 shadow-lg " onClick={scrollToUp}>
+      <h1 className="text-white text-3xl font-bold flex justify-center m-5 ">
+        Muhammed Emin Beköz To Do App
+      </h1>
     </div>
   );
 }
